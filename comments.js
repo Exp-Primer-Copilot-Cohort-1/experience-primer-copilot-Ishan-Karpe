179de9c7e955bd4851592a978987f637207170a6
Ishan Karpe <ishan.k.abhijeet@gmail.com>
2024-01-04 19:59:20 +0000

    extra

--- comments.js
+++ comments.js
@@ -21,7 +21,60 @@ const comments = [
 // Middleware
 app.use(express.json());
 
-// Create routes...
+// Create a route
+app.get("/", (req, res) => {
+    res.send("Hello World");
+  });
+  
+  // Create a route
+  app.get("/api/comments", (req, res) => {
+    res.send(comments);
+  });
+  
+  // Create a route
+  app.get("/api/comments/:id", (req, res) => {
+    const comment = comments.find((c) => c.id === parseInt(req.params.id));
+    if (!comment) {
+      res.status(404).send("Comment not found");
+    } else {
+      res.send(comment);
+    }
+  });
+  
+  // Create a route
+  app.post("/api/comments", (req, res) => {
+    const comment = {
+      id: comments.length + 1,
+      name: req.body.name,
+      comment: req.body.comment,
+    };
+    comments.push(comment);
+    res.send(comment);
+  });
+  
+  // Create a route
+  app.put("/api/comments/:id", (req, res) => {
+    const comment = comments.find((c) => c.id === parseInt(req.params.id));
+    if (!comment) {
+      res.status(404).send("Comment not found");
+    } else {
+      comment.name = req.body.name;
+      comment.comment = req.body.comment;
+      res.send(comment);
+    }
+  });
+  
+  // Create a route
+  app.delete("/api/comments/:id", (req, res) => {
+    const comment = comments.find((c) => c.id === parseInt(req.params.id));
+    if (!comment) {
+      res.status(404).send("Comment not found");
+    } else {
+      const index = comments.indexOf(comment);
+      comments.splice(index, 1);
+      res.send(comment);
+    }
+  });
 
 // Create a server
 app.listen(port, () => {
